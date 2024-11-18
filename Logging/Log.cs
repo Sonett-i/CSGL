9e3c3c663d1cad787c717a52f4065290eaeed2e1
sonett-i <sam@sonetti.net>
2024-11-18 12:25:46 +1030

    logging

---
+++ Logging/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Log
+{
+	public class Log
+	{
+		public static void Default(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		public static void Error(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
