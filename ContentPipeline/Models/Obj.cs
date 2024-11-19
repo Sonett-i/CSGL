f647607e918f044a8e9eb273bd86e64a384e33c0
sonett-i <sam@sonetti.net>
2024-11-19 22:24:41 +1030

    prep obj import

--- ContentPipeline/Models/Obj.cs
+++ ContentPipeline/Models/Obj.cs
@@ -8,8 +8,12 @@ namespace ContentPipeline.Extensions
 	{
 		public static void Import(string filePath)
 		{
-			
+			AssimpContext context = new AssimpContext();
 
+			Scene scene = context.ImportFile(filePath, PostProcessSteps.Triangulate | PostProcessSteps.GenerateNormals);
+
+			List<Assimp.Mesh> meshes = scene.Meshes;
+			List<Assimp.Material> materials = scene.Materials;
 
 
 			//return meshes.ToArray();
